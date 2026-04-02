82e2974960527ea275753188db82c48b913d9d4f
koushik6336 <136243534+koushik6336@users.noreply.github.com>
2026-04-02 13:26:44 +0530

    Day1 completed - Spring Boot + Docker

---
+++ src/main/java/com/devopsaiops/pipeline/HealthController.java
@@ -0,0 +1,13 @@
+package com.devopsaiops.pipeline;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "Application is Healthy ";
+    }
+}
---
+++ src/main/java/com/devopsaiops/pipeline/HelloController.java
@@ -0,0 +1,13 @@
+package com.devopsaiops.pipeline;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "DevOps + AIOps Pipeline Running ";
+    }
+}
